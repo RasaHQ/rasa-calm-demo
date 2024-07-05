d0b826034c3abac3065a8e06aa800503a4b8aa68
Greg Stephens <greg@udon.org>
2024-07-05 09:21:34 -0700

    updates with 3.8

---
+++ actions/select_card.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Any, Dict, List, Text
+from datetime import datetime
+
+from rasa_sdk import Action
+
+logger = logging.getLogger(__name__)
+
+class ActionShowSlots(Action):
+    def name(self):
+        return "select_card"
+
+    def run(self, dispatcher, tracker, domain):
+        events = []
+        cards = tracker.get_slot("cards")
+        if not cards:
+            dispatcher.utter_message(text="No cards found.")
+        else:
+            buttons = []
+            for card in cards:
+                buttons.append(
+                    {
+                        "title": card.get("name"),
+                        "payload": card.get("name"),
+                        # "payload": f"/SetSlots(current_card_name={card.get('name')}, current_card_number={card.get('number')})"
+                    }
+                )
+            dispatcher.utter_message(response="utter_select_card", buttons=buttons)
+        return events
