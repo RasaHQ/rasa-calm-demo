e98058f5fc26d24359d555ab373de5ce49995f2f
vcidst <hello@shailendra.me>
2024-09-06 14:27:21 +0200

    voice: set call metadata to slots

---
+++ actions/action_session_start.py
@@ -0,0 +1,23 @@
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ActionSessionStart(Action):
+
+    def name(self) -> str:
+        return "action_session_start"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: dict) -> list:
+        # get the metadata from the tracker
+        metadata = tracker.get_slot("session_started_metadata")
+        logger.info(f"🤙 action_session_start's metadata: {metadata}")
+
+        # set appropriate slots
+        return [
+            SlotSet("user_phone", metadata.get("user_phone")),
+            SlotSet("bot_phone", metadata.get("bot_phone")),
+        ]
